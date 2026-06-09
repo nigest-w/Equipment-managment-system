c2a12603ed064a31c60d44a4839c42a0b82f8ddb
Anbesa <Anbesa@ITS-PC-0025.LIB5.COM>
2026-06-09 14:38:13 +0300

    new change

--- server/config/database.js
+++ server/config/database.js
@@ -1,11 +1,24 @@
-const { Pool } = require("pg");
+require('dotenv').config();
+const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "equipment_db",
-  password: "nigest11",
-  port: 5432,
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
 });
-
 module.exports = pool;
+
+
+// const { Pool } = require("pg");
+
+// const pool = new Pool({
+//   user: "postgres",
+//   host: "localhost",
+//   database: "equipment_db",
+//   password: "nigest11",
+//   port: 5432,
+// });
+
+// module.exports = pool;
